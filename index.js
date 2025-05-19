85acf995c355707362f29fd8494a921a65a4fd11
Scot S <scotschroeder@gmail.com>
2025-05-19 14:53:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function testFunction(input1, input2) {
-    console.log('input1', input2);
+    console.log('input1', input1);
     console.log('input2', input2);
     return {
         "value1": input1 * 2,
