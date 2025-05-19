64082b172c9fcd63b74702050b1a58684eaa1d65
Scot S <scotschroeder@gmail.com>
2025-05-19 12:56:12 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+export function testFunction(input1, input2) {
+    console.log('input1', input2);
+    console.log('input2', input2);
+    return {
+        "value1": input1 * 2,
+        "value2": input2 * 2
+    }
+}
